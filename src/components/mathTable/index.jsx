e2e66b0a2330e21942463e207a7dc0d7b78b883e
John Hurt <rita.uy56@gmail.com>
2021-02-14 17:23:56 +1100

    before luke

--- src/components/mathTable/index.jsx
+++ src/components/mathTable/index.jsx
@@ -1,75 +1,90 @@
 import React from 'react';
 
 import { TextBox } from '../input'
-import { StyledNavList, StyledListItem, MathDisplay, Exit, Close } from './mathTableStyling'
+import { StyledNavList, StyledListItem, MathBody, Exit, Close, Variable } from './mathTableStyling'
 
-export class MathTable extends React.Component {
+export class MathTable extends React.Component 
+{
+  constructor(props){
+    super(props);
+    console.log("consturctor")
+    this.state = {
+      //inputNumber: '',
+      inputNumbers: [],
+      unsavedInputNumber: '',
+      display: false,
+      textBoxError: false, 
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    constructor(props){
-      super(props);
-      console.log("consturctor")
-      this.state = {
-        inputNumber: '',
-        unsavedInputVal: '',
-        display: false,
-      };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
+  handleChange(event) {
+    this.setState({
+      unsavedInputNumber: event.target.value
+    })
+    if(event.target.value <= 999 && event.target.value >= 0){
       this.setState({
-        unsavedInputVal: event.target.value
+        textBoxError: !this.state.textBoxError
       })
-      console.log("changed: " + this.state.unsavedInputVal);
-    }
-  
-    handleSubmit(event) {
-      console.log("submitted");
-
-      const re = /^[0-9\b]+$/;
-      if (re.test(this.state.unsavedInputVal)) {
-        this.setState({
-          inputNumber: this.state.unsavedInputVal
-        })
-      }
-      //event.preventDefault();
-    }
-  
-    multiplication () {
-      // No input, no answers...
-      const inputValue = (this.state.inputNumber || 0);
-  
-      var itterate = 1;
-      let displayArray;
-      
-      const answers = [];
-      //return all the answers
-      for(let i = 1; i <= 12; i++){
-        answers.push(inputValue * i);
-      }
-  
-      displayArray = answers.map(function(arrayAnswers){
-        return <StyledListItem>{inputValue} X {itterate++} = {arrayAnswers}</StyledListItem>
-      })
-      
-      return displayArray
-    }
-  
-    render(){
-
-      let results = this.multiplication();
-      
-      return(
-        <MathDisplay>
-          <TextBox value={ this.state.unsavedInputVal } onChange={this.handleChange} onSubmit={this.handleSubmit}/>
-          <StyledNavList>
-            <Exit>
-              <Close>X</Close>
-            </Exit>
-            {results}
-          </StyledNavList>
-        </MathDisplay>
-      )
     }
   }
+
+  handleSubmit(event) {
+    console.log("submitted");
+
+    const re = /^[0-9\b]+$/;
+    if (re.test(this.state.unsavedInputNumber)) {
+      this.setState({
+        inputNumber: this.state.unsavedInputNumber,
+        display: true
+      })
+    }
+    //event.preventDefault();
+  }
+  
+  drawTextBox() {
+    return (
+      <TextBox value={ this.state.unsavedInputNumber } onChange={this.handleChange} onSubmit={this.handleSubmit}/>
+    )
+  }
+  multiplication() {
+    // No input, no answers...
+    const recievedNumber = (this.state.inputNumber || 0);
+    var itterate = 1;
+    let displayArray;
+
+    const answers = [];
+    //return all the answers
+    for(let i = 1; i <= 12; i++){
+      answers.push(recievedNumber * i);
+    }
+    
+    if(this.state.display){
+      displayArray = answers.map(function(arrayAnswers){
+        return <StyledListItem> {recievedNumber} X {itterate++} = {arrayAnswers}</StyledListItem>
+      })
+    }
+    return displayArray
+  }
+
+  render() {
+    let results = this.multiplication();
+    return(
+      <MathBody>
+        {this.drawTextBox()}
+        {
+          this.state.inputNumber && (
+            <StyledNavList>
+              <Exit>
+                <Close>X</Close>
+              </Exit>
+              <Variable>{this.state.inputNumber}</Variable>
+              {results}
+            </StyledNavList>
+          )
+        }
+      </MathBody>
+    )
+  }
+}
--- src/components/mathTable/mathTableStyling.jsx
+++ src/components/mathTable/mathTableStyling.jsx
@@ -4,9 +4,11 @@ export const StyledNavList = styled.ul`
   background-color: #e0f1f5;
   position: absolute;
   padding: 0px 0px 10px 0px; 
-  list-style-type: none;
   border: 2px solid black;
   border-radius: 5px;
+  list-style-type: none;
+  width: 200px;
+  max-width: 200px;
 `
 
 export const StyledListItem = styled.li`
@@ -14,18 +16,28 @@ export const StyledListItem = styled.li`
   font: bold 15px Verdana, sans-serif;
   
 ` 
-export const MathDisplay = styled.div`
+export const MathBody = styled.div`
 
 `
 export const Exit = styled.div`
-  position: relative;
-  text-align: right;
-
+  display: flex;
   margin: 0px;
-  padding: 10px;
+  //padding: 10px;
 `
 export const Close = styled.h1`
-  font: bold 25px Verdana, sans-serif;
-  color: #4F6D7A;
   margin: 0px;
+  margin-right: 2px;
+  margin-left: auto;
+  padding: 2px;
+  font:  20px Verdana, sans-serif;
+  color: #4F6D7A;
+  &:hover{
+    background-color: #FBB13C;
+    color: white;
+  }
+`
+export const Variable = styled.h1`
+  padding: 0px 0px 5px 20px;
+  margin: 0px;
+  
 `
