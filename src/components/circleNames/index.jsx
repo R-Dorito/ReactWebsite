00e92b5a009e7c3929c92e3bf30006e30897daa5
John Hurt <rita.uy56@gmail.com>
2021-02-18 11:06:18 +1100

    circle

--- src/components/circleNames/index.jsx
+++ src/components/circleNames/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
+const circleDiv = styled.div`
+  /* padding: 10px;
+  margin: 20px;
+  display: "inline-block";
+  background-color: #5ec55e;
+  border-radius: "50%";
+  width: 100px;
+  height: 100px; */
+  padding: 500px;
+  border-color: #af0000;
+`;
 export class CircleText extends React.Component {
   render() {
-    return ();
+    return <circleDiv>ttt</circleDiv>;
   }
 }
