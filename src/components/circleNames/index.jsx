3da568c08d5dfdef49540601e50a9bf51153de17
John Hurt <rita.uy56@gmail.com>
2021-02-19 09:14:10 +1100

    Moved circle to new line

--- src/components/circleNames/index.jsx
+++ src/components/circleNames/index.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import styled from "styled-components";
 import { GlobalHeader, GlobalSubHeader } from "../globalStylings";
 
-const CircleDiv = styled.div`
-  margin-top: 10px;
+const DrawCircle = styled.div`
+  margin: 10px 10px 10px 0px;
   //border: 5px solid #000000;
   background-color: ${(props) => props.colorCircle || "white"};
   border-radius: 100%;
   width: 100px;
   height: 100px;
+  display: block;
+  flex-direction: column;
+  //flex-wrap: wrap;
+  float: left;
 `;
 
+const CircleBody = styled.div`
+  display: block; //why it not appear in new line?????
+  width: 100%;
+  float: left;
+`;
+
+export const CircleText = (props) => {
+  return <GlobalHeader>Draw a circle</GlobalHeader>;
+};
+
 export class Circle extends React.Component {
+  resetButton(isPressed) {}
+
   rgbGenerator() {
     var num1 = Math.random() * 255 + 1;
     var num2 = Math.random() * 255 + 1;
@@ -30,16 +46,17 @@ export class Circle extends React.Component {
     var rgbValue = this.rgbGenerator();
     var hslValue = this.hslGenerator();
 
+    var circles = [];
+    for (var i = 0; i <= 5; i++) {
+      circles.push(<DrawCircle colorCircle={rgbValue} />);
+    }
     return (
       <>
         <GlobalSubHeader>Random circles</GlobalSubHeader>
-        <CircleDiv colorCircle={rgbValue} />
-        <CircleDiv colorCircle={hslValue} />
+
+        <DrawCircle colorCircle={hslValue} />
+        <CircleBody>{circles}</CircleBody>
       </>
     );
   }
 }
-
-export const CircleText = (props) => {
-  return <GlobalHeader>Draw a circle</GlobalHeader>;
-};
--- src/components/globalStylings/index.jsx
+++ src/components/globalStylings/index.jsx
@@ -6,3 +6,10 @@ export const GlobalHeader = styled.h1`
   color: #000000;
   font: bold 25px Verdana, sans-serif;
 `;
+
+export const GlobalSubHeader = styled.h2`
+  margin: 0;
+  padding: 0;
+  color: #2f3253;
+  font: 20px Verdana, sans-serif;
+`;
