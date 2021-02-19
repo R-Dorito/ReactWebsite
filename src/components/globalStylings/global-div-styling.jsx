e6a8af41da4cac4db75007303b3bdff9175f6602
John Hurt <rita.uy56@gmail.com>
2021-02-19 13:45:44 +1100

    include tags 2

---
+++ src/components/globalStylings/global-div-styling.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+//this is used to display blocks in a new line, when window is small
+export const GlobalDivSeperator = styled.div`
+  display: block;
+  float: left;
+  flex-direction: row;
+`;
---
+++ src/components/globalStylings/global-text-styling.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const GlobalHeader = styled.h1`
+  margin: 0;
+  padding: 0;
+  color: #000000;
+  font: bold 25px Verdana, sans-serif;
+`;
+
+export const GlobalSubHeader = styled.h2`
+  margin: 0;
+  padding: 0;
+  color: #2f3253;
+  font: 20px Verdana, sans-serif;
+`;
+
+export const GlobalParagraph = styled.p`
+  font: 18px serif;
+  color: #3b3b3b;
+`;
+
+export const GlobalHyperlink = styled.a`
+  font: 18px serif;
+  color: #3b3b3b;
+  text-decoration: none;
+`;
